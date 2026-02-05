230a6bfc3e82fe02a24164441383d01b8608caa1
WONGZiF <100257722+WONGZiF@users.noreply.github.com>
2026-02-05 12:53:07 +0800

    Delete lab_1 directory

--- lab_1/Assignment_1_Python_Exercises/List_Iteration.py
+++
@@ -1,9 +0,0 @@
-# Write a program to find all factors of the numbers in the list l
-l = [52633, 8137, 1024, 999]
-# your code here
-for x in l:
-    print(f"Factors of {x}:")
-    for i in range(2, x):
-        if x % i == 0:
-            print(i)
-    print("---------------------")
